aba79dd36de530312d69c60f27d9868af0981828
chixyang <chixyang@gmail.com>
2013-12-26 23:15:17 +0800

    Update dbio.h

--- dbio.h
+++ dbio.h
@@ -2,11 +2,19 @@
  * 数据库操作函数定义
  */
  
+ #ifndef DBIO_H
+ #define DBIO_H
+ 
  #include <math.h>
+ #include "debug.h"
  
  //浮点数运算误差范围内的最小值
  #define eps 1e-10
  
  //地球半径，单位m
  #define EARTH_RADIUS 63781370
+ 
+ 
+ 
+ #endif
  
