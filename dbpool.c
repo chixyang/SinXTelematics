4665c370b1fa58cdd7c937f1eb6c4f402bcfa1f1
chixyang <chixyang@gmail.com>
2013-12-27 15:44:28 +0800

    Update dbpool.c

--- dbpool.c
+++ dbpool.c
@@ -1,5 +1,30 @@
 
-
 /**
  * 数据库池函数实现文件
  */
+
+#include "dbpool.h"
+
+//数据库池结构
+struct DBpool
+{
+  dbBusyList *busylist;
+  int busy_size;
+  dbIdleList *idlelist;
+  int idle_size;
+};
+
+//忙碌表结构
+struct DBBusyList
+{
+  MYSQL * db_link;
+  struct DBBusyList *next;
+};
+
+//空闲表结构
+struct DBIdleList
+{
+  MYSQL * db_link;
+  struct DBIdleList *next;
+};
+
