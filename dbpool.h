48b6b3dfcf4d22b59ba585a7054e051578294d29
chixyang <chixyang@gmail.com>
2013-12-27 20:09:57 +0800

    Update dbpool.h

--- dbpool.h
+++ dbpool.h
@@ -7,8 +7,17 @@
 
 #include "debug.h"
 
+//服务器名
+static const char* server;
+//用户名
+static const char* username;
+//密码
+static const char* password;
+//数据库名
+static const char* database;
+
 //数据库池节点
-struct DBpool dbpool;
+static struct DBpool *dbpool;
 
 //忙碌列表类型
 typedef struct DBBusyList dbBusyList;
