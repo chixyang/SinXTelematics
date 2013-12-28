e7211f5388ae6826a2d07aa03b9aa798f2b24106
chixyang <chixyang@gmail.com>
2013-12-28 21:39:01 +0800

    Update threadpool.h

--- threadpool.h
+++ threadpool.h
@@ -21,14 +21,14 @@ typedef struct Task  threadtask;
 typedef struct ThreadPool threadpool;
 
 //线程池指针
-static threadpool *pool;  //最后开始时初始化为NULL
+static threadpool *tdpool;  //最后开始时初始化为NULL
 
 /**
  * 线程池初始化函数
  * @param max_thread_num：要产生的最大线程数量
  * @return 实际创建的线程数目,-1表示初始化失败
  */
-int pool_init (int max_thread_num);
+int tdpool_init (int max_thread_num);
 
 /**
  * 向线程池中添加任务函数
@@ -37,14 +37,14 @@ int pool_init (int max_thread_num);
  * @return 0 ：成功
  *         -1：失败
  */
-int pool_add_task(void *(*process) (void *arg), void *arg);
+int tdpool_add_task(void *(*process) (void *arg), void *arg);
 
 /**
  * 线程池清除函数，清除任务队列，释放所有线程并回收资源，未执行完的线程等待其执行完
  * @return 0 成功
  *        -1 失败，也表示之前已被销毁
  */
-int pool_destroy();
+int tdpool_destroy();
 
 /**
  * 线程日常工作函数
@@ -58,7 +58,7 @@ void *thread_routine (void *arg);
  * @param add_num 新加线程的数目
  * @return 加成功线程的数目，-1表示线程池存在问题
  */
-int pool_add_thread(int add_num);
+int tdpool_add_thread(int add_num);
 
 
 
