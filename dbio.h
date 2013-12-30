d6c7819ac6a29d668720e1f396acafb8316274ae
chixyang <chixyang@gmail.com>
2013-12-30 17:44:33 +0800

    Update dbio.h

--- dbio.h
+++ dbio.h
@@ -14,6 +14,16 @@
  //地球半径，单位m
  #define EARTH_RADIUS 63781370
  
+ //设置字符编码为utf8
+ #define setUTF8(x)    do{                                               \
+                             if(mysql_query(x,"set names \'utf8\'"))     \
+	                                 {                                      \
+		                                    perror("set utf8 error");          \
+		                                    recycleConn(x);                    \
+		                                    return -1;                         \
+	                                 }                                      \
+	                          while(0)
+ 
  
  
  #endif
